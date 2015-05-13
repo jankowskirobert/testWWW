9f92827d1f2569f3bb0258ada219e46a586ceceb
Robert Jankowski <robert.jankowski@abastra.com>
2015-05-13 11:59:05 +0200

    test 5

--- DriverLoad.py
+++ DriverLoad.py
@@ -13,7 +13,7 @@ class DriverLoad:
 
     def __init__(self, param):
         self.driver = webdriver.Remote(
-   command_executor='http://127.0.0.1:4445/wd/hub',
+   command_executor='http://127.0.0.1:4444/wd/hub',
    desired_capabilities=DesiredCapabilities.FIREFOX)
         self.driver.get(param)
         
