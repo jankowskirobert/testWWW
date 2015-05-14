b7cb6656aa27f07981b1d10ef60bfe9a8d4c832d
Robert Jankowski <robert.jankowski@abastra.com>
2015-05-14 10:28:36 +0200

    Test 67X

--- DriverLoad.py
+++ DriverLoad.py
@@ -12,7 +12,7 @@ class DriverLoad:
     'Return driver'
 
     def __init__(self, param):
-        self.driver = webdriver.Remote(command_executor="http://172.21.2.128:5555/wd/hub",desired_capabilities={ "browserName": "firefox", "platform": "LINUX",})
+        self.driver = webdriver.Remote(command_executor="http://127.0.0.1:4444/wd/hub",desired_capabilities={ "browserName": "firefox", "platform": "LINUX",})
         self.driver.get(param)
 
 #         Remote(
