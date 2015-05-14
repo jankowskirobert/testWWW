c65d6b8b4cd01ee405a492d783ef80ded312d157
Robert Jankowski <robert.jankowski@abastra.com>
2015-05-14 09:53:04 +0200

    Test 66XX

--- DriverLoad.py
+++ DriverLoad.py
@@ -12,9 +12,11 @@ class DriverLoad:
     'Return driver'
 
     def __init__(self, param):
-        self.driver = webdriver.Remote(
-   command_executor='http://127.0.0.1:4444/wd/hub',
-   desired_capabilities=DesiredCapabilities.FIREFOX)
+        self.driver = webdriver.Firefox()
+
+#         Remote(
+#    command_executor='http://127.0.0.1:4445/wd/hub',
+#    desired_capabilities=DesiredCapabilities.FIREFOX)
         self.driver.get(param)
         
     def importDriver(self):
