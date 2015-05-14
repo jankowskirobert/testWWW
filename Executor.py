8ba50ab484568987a2387b1b52ab0cbdf0dc3bf7
Robert Jankowski <robert.jankowski@abastra.com>
2015-05-14 09:52:25 +0200

    Test 66X

---
+++ Executor.py
@@ -0,0 +1,15 @@
+'''
+Created on May 13, 2015
+
+@author: tester
+'''
+import subprocess
+
+def fooReturnFive():
+    return 5
+def fooReturnSix():
+    return 6
+
+#
+
+subprocess.call("echo","\"HEHE\"")
---
+++ TestExecutor.py
@@ -0,0 +1,12 @@
+'''
+Created on May 13, 2015
+
+@author: tester
+'''
+import pytest
+import Executor
+
+def test_five():
+    assert Executor.fooReturnFive == 5
+def test_six():
+    assert Executor.fooReturnFive == 6
