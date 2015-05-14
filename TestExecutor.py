d15f3faaf3285ca4ed838cf9a78ef1107fc23c7e
Robert Jankowski <robert.jankowski@abastra.com>
2015-05-14 13:01:46 +0200

    aaa

--- TestExecutor.py
+++ TestExecutor.py
@@ -4,9 +4,11 @@ Created on May 13, 2015
 @author: tester
 '''
 import pytest
-import Executor
+from Executor import fooReturnFive, fooReturnSix
+
+
 
 def test_five():
-    assert Executor.fooReturnFive == 5
+    assert fooReturnFive == 5
 def test_six():
-    assert Executor.fooReturnFive == 6
+    assert fooReturnSix == 6
